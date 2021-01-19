4c019574582c59e0c92c9852df7b159d992dca52
Tommi Mäkelä <tmmi.makela@outlook.com>
2021-01-19 17:20:33 +0200

    Requirements 3 and 4 done

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -5,6 +5,7 @@ import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
+  const [showPersons, setShowPersons] = useState([])
   const [sortBtnValue, setSortBtnValue] = useState(false)
 
   useEffect(() => {
@@ -15,13 +16,29 @@ const App = () => {
       )
   }, [])
 
-  console.log('persons length:', persons.length)
-  console.log(persons)
 
-  console.log('active:', sortBtnValue)
+  const handleFilter = (event) => {
+    setShowPersons(persons.filter((person, i) =>
+      persons[i].name.includes(event.target.value)
+    ))
+  }
 
   return(
     <div>
+
+      <div>
+        <div>
+          <h2>Total amount of all the names: </h2>
+          <h2> {persons.reduce((sum, person) => sum + person.amount, 0)} </h2>
+        </div>
+        <div>
+          <h2>Amount of the name: { showPersons.length === 1 ? showPersons[0].amount : '' } </h2>
+          <input 
+            onChange={handleFilter}
+          />
+        </div>
+      </div>
+
       <button onClick={() => setSortBtnValue(!sortBtnValue)} > 
         {sortBtnValue ? 'Sort by amount' : 'Sort by name'} 
       </button>
@@ -32,6 +49,7 @@ const App = () => {
           persons.sort(function(a, b){return b.amount - a.amount})
         } 
       />
+
     </div>
   )
 }
