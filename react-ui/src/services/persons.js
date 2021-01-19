71889c8d8868c1ced6222d889926d2fdb1360cd2
Tommi-dev <55761663+Tommi-dev@users.noreply.github.com>
2021-01-19 21:13:15 +0200

    Delete react-ui directory

--- react-ui/src/services/persons.js
+++
@@ -1,9 +0,0 @@
-//const baseUrl = 'http://localhost:3001/api/persons'
-const baseUrl = '/api/persons'
-
-const getAll = async () => {
-  const response = await fetch(baseUrl)
-  return response.json()
-}
-
-export default { getAll }
