7eeb4c89020f47c39a24ba118b6837b8000e40c9
Tommi Mäkelä <tmmi.makela@outlook.com>
2021-01-19 11:01:27 +0200

    Requirements 1 and 2 done

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -5,6 +5,7 @@ import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
+  const [sortBtnValue, setSortBtnValue] = useState(false)
 
   useEffect(() => {
     personService
@@ -17,9 +18,20 @@ const App = () => {
   console.log('persons length:', persons.length)
   console.log(persons)
 
+  console.log('active:', sortBtnValue)
+
   return(
     <div>
-      <Persons persons={persons} />
+      <button onClick={() => setSortBtnValue(!sortBtnValue)} > 
+        {sortBtnValue ? 'Sort by amount' : 'Sort by name'} 
+      </button>
+
+      <Persons 
+        persons={ sortBtnValue ? 
+          persons.sort(function(a, b){return a.name.localeCompare(b.name)}) :
+          persons.sort(function(a, b){return b.amount - a.amount})
+        } 
+      />
     </div>
   )
 }
